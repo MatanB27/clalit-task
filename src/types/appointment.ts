5c9f8e7f9f93d8094db5c6893d91c83669f8dcbc
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2026-04-16 20:07:24 +0300

    adding types

---
+++ src/types/appointment.ts
@@ -0,0 +1,20 @@
+export type SpecialtyId = 'family-medicine' | 'dermatology' | 'gynecology';
+
+export interface Specialty {
+  id: SpecialtyId;
+  label: string;
+};
+
+export interface AppointmentSlot {
+  specialtyId: SpecialtyId;
+  date: string;
+  time: string;
+};
+
+export interface Appointment {
+  specialtyId: SpecialtyId;
+  specialtyLabel: string;
+  date: string;
+  time: string;
+  patientName: string;
+};
