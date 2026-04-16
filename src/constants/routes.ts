d98c872e2429f971e5b5dec4c93b82947d382c9d
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2026-04-16 20:02:54 +0300

    Adding constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  LOGIN: 'Login',
+  HOME: 'Home',
+  CALENDAR: 'Calendar',
+  SUMMARY: 'Summary',
+} as const;
---
+++ src/constants/storage-keys.ts
@@ -0,0 +1,5 @@
+export const STORAGE_KEYS = {
+  AUTH_TOKEN: 'auth-token',
+  AUTH_USER: 'auth-user',
+  ACTIVE_APPOINTMENT: 'active-appointment',
+} as const;
